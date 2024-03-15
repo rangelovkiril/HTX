bb1fcb11205f3742d2f71f2fd8ebd11a89ca13ef
Darian <darianganev83@gmail.com>
2024-03-16 01:22:31 +0200

    footer commit

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -11,15 +11,5 @@ const Footer = () => {
     </footer>
   );
 };
-    return (
-        <footer className={styles.footer}>
-            <div>
-                <a href="https://www.instagram.com/df4s.2024/">Instagram</a>
-                <a href="https://mail.google.com/mail/u/5/#inbox">Gmail</a>
-                <a href="https://www.facebook.com/">Facebook</a>
-            </div>
-        </footer>
-    )
-}
 
-export default Footer
+export default Footer;
