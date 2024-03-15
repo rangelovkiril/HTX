1d726501a03be8accceda97c7fc8cbe80567d926
Darian <darianganev83@gmail.com>
2024-03-15 12:31:02 +0200

    Revert "Refactor front-end architecture"
    This reverts commit e927b4d8db69e0486d0e6a3e318f855b8826dabf.

--- src/pages/subjects.tsx
+++ src/pages/subjects.tsx
@@ -4,16 +4,6 @@ import Footer from "../pages/Footer";
 import Link from "next/link";
 import { Box, Button, Typography, Grid } from "@mui/material";
 
-<<<<<<< HEAD
-<<<<<<< HEAD:src/components/screens/Subjects/Subjects.tsx
-import styles from './subjects.module.scss'
-
-// TODO: Rename file and clear logic !!!
-
-=======
->>>>>>> parent of e927b4d (Refactor front-end architecture):src/pages/subjects.tsx
-=======
->>>>>>> parent of e927b4d (Refactor front-end architecture)
 const SubjectBox = ({ name }) => {
   return (
     <Box
@@ -80,29 +70,13 @@ const SubjectsPage = () => {
             flexWrap: "wrap",
           }}
         >
-<<<<<<< HEAD
-<<<<<<< HEAD:src/components/screens/Subjects/Subjects.tsx
-          {/* Map through subjects array */}
-=======
->>>>>>> parent of e927b4d (Refactor front-end architecture):src/pages/subjects.tsx
-=======
->>>>>>> parent of e927b4d (Refactor front-end architecture)
           {subjects.map((subject, index) => (
             <SubjectBox key={index} name={subject} />
           ))}
         </Box>
       </Box>
-<<<<<<< HEAD
-<<<<<<< HEAD:src/components/screens/Subjects/Subjects.tsx
-    </div>
-=======
       <Footer />
     </>
->>>>>>> parent of e927b4d (Refactor front-end architecture):src/pages/subjects.tsx
-=======
-      <Footer />
-    </>
->>>>>>> parent of e927b4d (Refactor front-end architecture)
   );
 };
 
