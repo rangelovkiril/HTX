b3649cd23b51cf4c2a242a6f352d612ad9fb2805
Boris Felix <bobyfeliks@gmail.com>
2024-03-15 15:17:10 +0200

    improving the page style

--- src/app/ConnectWallet.tsx
+++ src/app/ConnectWallet.tsx
@@ -6,6 +6,7 @@ declare var window: any
 
 import { ethers } from 'ethers'
 // import factory from typechain
+import './mmauth_style.css'
 
 export default function ConnectWallet() {
   // State to store and show the connected account
