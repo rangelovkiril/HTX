154634068632d27b66b36a642f1185cf45ee1735
Boris Felix <bobyfeliks@gmail.com>
2024-03-15 13:25:20 +0200

    styling changes

--- src/app/ConnectWallet.tsx
+++ src/app/ConnectWallet.tsx
@@ -5,12 +5,12 @@ import React, {useState, useEffect} from 'react'
 declare var window: any
 
 import { ethers } from 'ethers'
-// import factory from typechain  
+// import factory from typechain
 
 export default function ConnectWallet() {
   // State to store and show the connected account
   const [connectedAccount, setConnectedAccount] = useState('null');
-    
+
   async function connectMetamask() {
 
     if (window.ethereum) {
@@ -26,10 +26,9 @@ export default function ConnectWallet() {
   }
 
   return (
-  <div className=''>
+  <div className='button'>
     <button className='' onClick={() => connectMetamask()}>Connect to Metamask</button>
   </div>
 
   );
-}
-
+} 
